e6ce9469c1054af0ef474681febe0eae1b45345b
bodya <lavajover@gmail.com>
2025-02-06 19:05:35 +0300

    feat: add config file and reader

--- api-gateway/internal/config/config.go
+++ api-gateway/internal/config/config.go
@@ -1,2 +1,40 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type APIConfig struct{
+	Env string `yaml:"env"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct{
+	Host string `yaml:"host"`
+	Port string `yaml:"port"`
+}
+
+func MustLoad() *APIConfig{
+	configPath := os.Getenv("API_CONFIG_PATH")
+
+	if configPath == ""{
+		log.Fatalf("API_CONFIG_PATH was not found\n")
+	}
+
+	if _, err := os.Stat(configPath); err != nil{
+		log.Fatalf("failed to load config file: %v\n", err)
+	}
+
+	var cfg APIConfig
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil{
+		log.Fatalf("failed to read config: %v\n", err)
+	}
+
+	return &cfg
+}
