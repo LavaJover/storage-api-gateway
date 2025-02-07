6d095188a24cf8f3cffa66c9e68bb37b27e77451
bodya <lavajover@gmail.com>
2025-02-07 21:19:18 +0300

    feat: add chain middleware

---
+++ api-gateway/pkg/middleware/chain.go
@@ -0,0 +1,12 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func ChainMiddleware(h http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h)
+	}
+	return h
+}
